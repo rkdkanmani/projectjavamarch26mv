39010c64a3894668be4ce57f60298607685ba46a
rkdkanmani <kanmanisupm91@gmail.com>
2026-02-22 16:20:14 +0530

    kanmani-work

--- src/org/zoho/SampleGitClass.java
+++ src/org/zoho/SampleGitClass.java
@@ -6,18 +6,24 @@ public class SampleGitClass {
 		
 		System.out.println("Git data1");
 		
-		
-		
-
-	}
+		}
 	 
-	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		gitData1();
 		int a1=443;
 		System.out.println(a1);
+		System.out.println("Branch A start the work");
+		String data="Test Data verified";
+		System.out.println(data);
+		long b= 7878;
+		if(b==565)
+		{
+			System.out.println("condition true");
+		}
 		
-		
-		
+		else
+		{
+			System.out.println("condition false");
+		}
 	}
 }

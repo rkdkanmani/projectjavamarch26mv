9c32170d99f4f18802930d775c8def4a60e43a19
rkdkanmani <kanmanisupm91@gmail.com>
2026-02-22 14:56:10 +0530

    done

---
+++ src/org/zoho/SampleGitClass.java
@@ -0,0 +1,23 @@
+package org.zoho;
+
+public class SampleGitClass {
+
+	public static void gitData1() {
+		
+		System.out.println("Git data1");
+		
+		
+		
+
+	}
+	 
+	
+	public static void main(String[] args) {
+		gitData1();
+		int a1=443;
+		System.out.println(a1);
+		
+		
+		
+	}
+}
